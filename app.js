6c3db706df1c49f857128f671a9b5449a35a37b7
premn123 <146005631+premn123@users.noreply.github.com>
2023-09-26 21:37:51 +0530

    added

---
+++ app.js
@@ -0,0 +1,13 @@
+const videoCardContainer = document.querySelector('.video-container');
+
+// search bar
+
+const searchInput = document.querySelector('.search-bar');
+const searchBtn = document.querySelector('.search-btn');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if(searchInput.value.length){
+        location.href = searchLink + searchInput.value;
+    }
+});
